633ec681a60f798cbb003e8e4ca855f1a80cca89
Githubuser289 <moldovan.mms@gmail.com>
2025-01-21 22:42:26 +0200

    second

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>proba proiect Vite + React pe GitHub pages</h1>
+      <h1>proiect sablon Vite + React pe GitHub pages</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
